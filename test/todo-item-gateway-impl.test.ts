76697fc47d784ef4bd261a4ef3e4b9bd4953e570
José Ronaldo de Oliveira Júnior <joseronaldobpg@gmail.com>
2022-09-10 22:53:05 -0300

    ✅ ensure gateway returns false when adapter throws

--- test/todo-item-gateway-impl.test.ts
+++ test/todo-item-gateway-impl.test.ts
@@ -1,9 +1,9 @@
-import { GatewayProtocol } from "@/core/gateway-adapter";
+import { GatewayProtocol as GatewayAdapter } from "@/core/gateway-adapter";
 import { TodoItem } from "@/core/todo-item";
 import { TodoItemGateway } from "@/core/todo-item-gateway";
 
 export class TodoItemGatewayImpl implements TodoItemGateway {
-  constructor(private gatewayAdapter: GatewayProtocol) {}
+  constructor(private gatewayAdapter: GatewayAdapter) {}
   save(todoItem: TodoItem) {
     try {
       debugger;
@@ -17,18 +17,32 @@ export class TodoItemGatewayImpl implements TodoItemGateway {
 }
 
 describe("TodoItemGatewayImpl", () => {
-  let mockGatewayProtocol: GatewayProtocol = {
+  const mockGatewayAdapter: GatewayAdapter = {
     save: jest.fn(),
   };
 
   it("should create an instance", () => {
-    expect(new TodoItemGatewayImpl(mockGatewayProtocol)).toBeTruthy();
+    expect(new TodoItemGatewayImpl(mockGatewayAdapter)).toBeTruthy();
   });
 
   it("should save a todo item", () => {
-    const todoItemGateway = new TodoItemGatewayImpl(mockGatewayProtocol);
+    const todoItemGateway = new TodoItemGatewayImpl(mockGatewayAdapter);
     const todoItem = new TodoItem("Test", false);
     const result = todoItemGateway.save(todoItem);
     expect(result).toBe(true);
+  });
+
+  it("should not save a todo item", () => {
+    const mockGatewayAdapterWithError: GatewayAdapter = {
+      save: jest.fn(() => {
+        throw new Error("Error");
+      }),
+    };
+    const todoItemGateway = new TodoItemGatewayImpl(
+      mockGatewayAdapterWithError
+    );
+    const todoItem = new TodoItem("Test", false);
+    const result = todoItemGateway.save(todoItem);
+    expect(result).toBe(false);
   });
 });
